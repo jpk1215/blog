7aee9484e68818b77f1179f552420517bf284ce8
Jeffrey Klaus <jpk1215@gmail.com>
2020-12-30 10:53:07 -0500

    finished styling

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import Head from 'next/head'
-import Layout, {siteTitle} from '../components/layout'
+import Link from 'next/link'
 import { getSortedPostsData } from '../lib/posts'
+import Layout, {siteTitle} from '../components/layout'
+import Date from '../components/date'
 import utilStyles from '../styles/Utils.module.css'
 
 export async function getStaticProps () {
@@ -30,11 +32,13 @@ export default function Home({ allPostsData }) {
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
-              {title}
+              <Link href={`/posts/${id}`}>
+                <a>{title}</a>
+              </Link>
               <br />
-              {id}
-              <br />
-              {date}
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
             </li>
           ))}
         </ul>
